3af6160045e8be3086cf3eef02dacadb15505b8c
tsavobc <tsavobc@hotmail.com>
2025-12-26 23:46:58 +0000

    File changes

--- functions/getStripePrices.ts
+++ functions/getStripePrices.ts
@@ -1,6 +1,8 @@
 import Stripe from 'npm:stripe@17.5.0';
 
-const stripe = new Stripe(Deno.env.get("STRIPE_SECRET_KEY"));
+const stripe = new Stripe(Deno.env.get("STRIPE_SECRET_KEY"), {
+    apiVersion: '2024-12-18.acacia'
+});
 
 Deno.serve(async (req) => {
     try {
