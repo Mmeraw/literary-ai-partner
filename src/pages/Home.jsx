192d95fa27b422da93c320cdc48ab9344f9d3472
tsavobc <tsavobc@hotmail.com>
2025-12-26 19:30:50 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -65,7 +65,7 @@ export default function Home() {
                             <Link to={createPageUrl('UploadManuscript')}>
                                 <Button size="lg" className="h-14 px-8 text-lg bg-gradient-to-r from-indigo-600 to-purple-600 hover:from-indigo-700 hover:to-purple-700 shadow-lg shadow-indigo-500/25">
                                     <BookOpen className="w-5 h-5 mr-2" />
-                                    Upload Full Work
+                                    Upload Your Writing
                                 </Button>
                             </Link>
                             <Link to={createPageUrl('Evaluate')}>
