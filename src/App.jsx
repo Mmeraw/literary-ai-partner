ee1c295d517fa441e7f45f4654e8f4892c078b4e
Mmeraw <tsavobc@hotmail.com>
2026-01-13 15:51:15 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import MvpLayout from "./layouts/MvpLayout.jsx";
 import MvpLanding from "./pages/MvpLanding.jsx";
 import MvpEvaluate from "./pages/MvpEvaluate.jsx";
 
@@ -7,8 +8,10 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MvpLanding />} />
-        <Route path="/evaluate" element={<MvpEvaluate />} />
+        <Route element={<MvpLayout />}>
+          <Route path="/" element={<MvpLanding />} />
+          <Route path="/evaluate" element={<MvpEvaluate />} />
+        </Route>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
