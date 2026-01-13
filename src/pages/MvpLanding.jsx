23af6dda4c356f8ed54592d2d2bd90dfd6e538df
Mmeraw <tsavobc@hotmail.com>
2026-01-13 16:01:38 -0700

    Update MvpLanding.jsx

--- src/pages/MvpLanding.jsx
+++ src/pages/MvpLanding.jsx
@@ -1,1 +1,10 @@
+export default function MvpLanding() {
+  return (
+    <div style={{ maxWidth: 900, margin: "40px auto", padding: 16 }}>
+      <h1>RevisionGrade MVP</h1>
+      <p>Go to <code>/evaluate</code> to run a 13-criteria evaluation.</p>
+      <a href="/evaluate">Go to Evaluate</a>
+    </div>
+  );
+}
 
