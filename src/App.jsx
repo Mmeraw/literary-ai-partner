4eac5e707deb5202bd0be1b47b1e34d04708f6f4
Mmeraw <tsavobc@hotmail.com>
2026-01-13 17:00:17 -0700

    Fix import: change MvpEvaluate to Evaluate

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MvpLayout from "./layouts/MvpLayout.jsx";
 import MvpLanding from "./pages/MvpLanding.jsx";
-import MvpEvaluate from "./pages/MvpEvaluate.jsx";
-
+import Evaluate from "./pages/Evaluate.jsx";
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MvpLayout />}>
           <Route path="/" element={<MvpLanding />} />
-          <Route path="/evaluate" element={<MvpEvaluate />} />
-        </Route>
+                        <Route path="/evaluate" element={<Evaluate />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
