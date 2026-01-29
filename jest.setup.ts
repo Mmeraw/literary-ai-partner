946983c4befe4209762979d8ffc0972ae360ac03
Mmeraw <tsavobc@hotmail.com>
2026-01-29 01:42:06 +0000

    chore: add diagnostic logging to verify secrets in workflow

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,3 +3,5 @@ process.env.SUPABASE_URL =
   process.env.SUPABASE_URL ?? "http://localhost:54321";
 process.env.SUPABASE_ANON_KEY =
   process.env.SUPABASE_ANON_KEY ?? "test_anon_key";
+process.env.SUPABASE_SERVICE_ROLE_KEY =
+  process.env.SUPABASE_SERVICE_ROLE_KEY ?? "test_service_role_key";
