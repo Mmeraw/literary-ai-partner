06a4085bf0cf76bb1049b45455ee91acbdf43884
Mmeraw <tsavobc@hotmail.com>
2026-06-04 18:40:48 +0000

    fix(storygate): correct industry auth css import paths

--- app/storygate-studio/industry/forgot-password/page.tsx
+++ app/storygate-studio/industry/forgot-password/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../storygate-studio.module.css';
+import styles from '../../storygate-studio.module.css';
 
 export default function StorygateStudioPage() {
   return (
